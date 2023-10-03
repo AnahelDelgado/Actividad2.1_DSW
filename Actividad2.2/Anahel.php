3c883a5ba16a18b21637aea0ee1f19f465ac0a96
Anahel Delgado <anahelginesdelgadoalmeida@alumno.ieselrincon.es>
2023-10-03 12:04:27 +0100

    Actualización en 'Anahel.php' y en 'recibe_datos.php'

--- Actividad2.2/Anahel.php
+++ Actividad2.2/Anahel.php
@@ -1,3 +1,26 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Anahel</title>
+    </head>
+    <body>
+        <form action="recibe_datos.php" method="get">
+        <br>Nombre: <input name="username" type="text" value="Usuario345">
+        <br>
+            <!-- Parte de select multiple -->
+        <label for="idioma">Elija su idioma: </label>
+        <select multiple name="idioma" id="idioma" size="4" value="idioma">
+            <option selected value="espa">Español</option>
+            <option value="ingl">Inglés</option>
+            <option value="cata">Catalán</option>
+            <option value="vasc">Vasco</option>
+        </select>
+        <br>
+        <p><input type="checkbox" required name="terms">Acepto las condiciones </p>
+        <br>
+        <input type="submit">
+        </form>
+    </body>
+</html>
--- Actividad2.2/recibe_datos.php
+++ Actividad2.2/recibe_datos.php
@@ -1,3 +1,12 @@
 <?php
 
+$username = $_GET['username'];
+$gender = $_GET['gender'];
+$language = $_GET['idioma'];
+$terms = $_GET['terms'];
+$language = $_GET['idioma'];
+
+echo "Hola $username";
+
+
 ?>
