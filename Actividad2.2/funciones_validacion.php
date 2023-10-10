a5f11917a273d6e3cb80cf3d50a35e91e11eff7e
Anahel Delgado <anahelginesdelgadoalmeida@alumno.ieselrincon.es>
2023-10-10 12:33:41 +0100

    Actualización de funciones_validadción

--- Actividad2.2/funciones_validacion.php
+++ Actividad2.2/funciones_validacion.php
@@ -1,16 +1,19 @@
 <?php
 
-    $patron_texto = "/^[a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜàèìòùÀÈÌÒÙ\s]+$/";
+    function validar_user_name($username){
 
-    if( empty($_POST['username']) )
-            $aErrores[] = "Debe especificar el nombre";
-        else
-        {
-            // Comprobar mediante una expresión regular, que sólo contiene letras y espacios:
-                if( preg_match($patron_texto, $_POST['username']) )
-                $aMensajes[] = "Nombre de usuario: [".$_POST['username']."]";
+        $patron_texto = "/^[a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜàèìòùÀÈÌÒÙ\s]+$/";
+
+        if( empty($_POST['username']) )
+                $aErrores[] = "Debe especificar el nombre";
             else
-                $aErrores[] = "El nombre sólo puede contener letras y espacios";
-        }
+            {
+                // Comprobar mediante una expresión regular, que sólo contiene letras y espacios:
+                    if( preg_match($patron_texto, $_POST['username']) )
+                    $aMensajes[] = "Nombre de usuario: [".$_POST['username']."]";
+                else
+                    $aErrores[] = "El nombre sólo puede contener letras y espacios";
+            }
+    }
 
 ?>
