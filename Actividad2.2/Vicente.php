e3aa0ed4646a2683caeddaa11feaa507d9911fb3
Vicente <33096355+Vicente015@users.noreply.github.com>
2023-10-20 11:44:05 +0100

    Implementar array de valores por defecto

--- Actividad2.2/Vicente.php
+++ Actividad2.2/Vicente.php
@@ -1,3 +1,28 @@
+<?php
+
+// Array de valores por defecto
+$valores_defecto = array(
+  "email" => "vicente@example.com",
+  "age" => "12",
+  "gender" => "frog",
+  "terms" => "checked"
+);
+
+// Array de los valores de gender
+$valores_gender = array(
+  "man" => "",
+  "female" => "",
+  "frog" => ""
+);
+
+// Establece selected solo en el valor por defecto
+$valores_gender[$valores_defecto["gender"]] = "selected";
+
+var_dump($valores_defecto);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,19 +39,19 @@
         <input type="hidden" name="nombre_formulario" id="nombre_formulario" value="vicente">
 
         <label for="email">Email: </label>
-        <input type="text" name="email" id="email" placeholder="example@example.com">
+        <input type="text" name="email" id="email" value="<?= $valores_defecto["email"] ?>">
 
         <label for="user">Edad: </label>
-        <input type="number" name="age" id="age" placeholder="00">
+        <input type="number" name="age" id="age" value="<?= $valores_defecto["age"] ?>">
 
         <label for="gender">Género: </label>
         <select name="gender" id="gender" multiple>
-          <option value="man" selected>Hombre</option>
-          <option value="female">Mujer</option>
-          <option value="frog">Rana</option>
+          <option value="man" <?= $valores_gender["man"] ?>>Hombre</option>
+          <option value="female" <?= $valores_gender["female"] ?>>Mujer</option>
+          <option value="frog" <?= $valores_gender["frog"] ?>>Rana</option>
         </select>
 
-        <input type="checkbox" name="terms" id="terms" checked>
+        <input type="checkbox" name="terms" id="terms" <?= $valores_defecto["terms"] ?>>
         <label for="terms">Acepto los términos de servicio y la política de privacidad.</label>
 
         <section>
