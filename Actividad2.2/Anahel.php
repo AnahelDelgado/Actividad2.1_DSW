1b066c9e6280f1452e38845c137410babb96a324
Anahel Delgado <anahelginesdelgadoalmeida@alumno.ieselrincon.es>
2023-10-20 11:50:48 +0100

    Modificación arrays en Anahel.php

--- Actividad2.2/Anahel.php
+++ Actividad2.2/Anahel.php
@@ -1,3 +1,25 @@
+<?php
+    //Ponemos como valor predeterminado el idioma Español
+    $array_valor_defecto = array("idiomaSelect" => "espa");
+
+    $select_espa = $select_ingl = $select_cata = $select_vasc = "";
+
+    switch($array_valor_defecto["idiomaSelect"]){
+        case "espa":
+            $select_espa = "selected";
+            break;
+        case "ingl":
+            $select_ingl = "selected";
+            break;
+        case "cata":
+            $select_cata = "selected";
+            break;
+        case "vasc":
+            $select_vasc = "selected";
+            break;
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -8,22 +30,22 @@
     <body>
         <form action="recibe_datos.php" method="post">
 
-        <input type="hidden" name="nombre_formulario" id="nombre_formulario" value="anahel">
+            <input type="hidden" name="nombre_formulario" id="nombre_formulario" value="anahel">
 
-        <br>Nombre: <input name="username" type="text" value="Usuario345">
-        <br>
-            <!-- Parte de select multiple -->
-        <label for="idioma">Elija su idioma: </label>
-        <select multiple name="idioma" id="idioma" size="4" value="idioma">
-            <option selected value="espa">Español</option>
-            <option value="ingl">Inglés</option>
-            <option value="cata">Catalán</option>
-            <option value="vasc">Vasco</option>
-        </select>
-        <br>
-        <p><input type="checkbox" required name="terms">Acepto las condiciones </p>
-        <br>
-        <input type="submit">
+            <br>Nombre: <input name="username" type="text" value="Usuario345">
+            <br>
+                <!-- Parte de select multiple -->
+            <label for="idioma">Elija su idioma: </label>
+            <select multiple name="idioma" id="idioma" value="idioma">
+                <option value="espa" <?= $select_espa ?>>Español</option>
+                <option value="ingl" <?= $select_ingl ?> >Inglés</option>
+                <option value="cata" <?= $select_cata ?> >Catalán</option>
+                <option value="vasc" <?= $select_vasc ?> >Vasco</option>
+            </select>
+            <br>
+            <p><input type="checkbox" required name="terms">Acepto las condiciones </p>
+            <br>
+            <input type="submit">
         </form>
     </body>
 </html>
