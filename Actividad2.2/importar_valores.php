63c131bb10dd7d4a29bf79a5ee348ced4d5a7e25
Vicente <33096355+Vicente015@users.noreply.github.com>
2023-10-20 12:28:43 +0100

    Crear funcion de insertar valores en array

--- Actividad2.2/importar_valores.php
+++ Actividad2.2/importar_valores.php
@@ -1,12 +1,34 @@
 <?php
+// véase: https://www.geeksforgeeks.org/how-to-parse-a-csv-file-in-php/
 
-if (($open = fopen("Book1.csv", "r")) !== false) {
+$valores = array();
+
+// Carga los valores desde un archivo CSV
+function cargar_defecto(string $nombre_archivo) {
+  global $valores;
+  // Abre el archivo CSV
+  if (($open = fopen($nombre_archivo, "r")) !== false) {
+    // Lee cada línea del CSV
     while (($data = fgetcsv($open, 1000, ",")) !== false) {
-        $array[] = $data;
+      $valores[] = $data;
     }
-
     fclose($open);
+  }
 }
 
+// Inserta los valores por defecto en el array pasado por parámetros
+function insertar_en_array() {
+  global $valores;
+  $array = array();
+
+  foreach ($valores as $clave => $valor) {
+    if (!$valor) continue;
+    $array[$valor[0]] = $valor[1];
+  }
+  return $array;
+}
+
+
+var_dump($valores);
 
 ?>
